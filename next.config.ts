946278a84e81fbfa2473e4856b602871a1034c06
bharat-khalate <bharatkhalate50@gmail.com>
2026-04-22 18:41:44 +0530

    fix: path update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx"],
+  basePath: "/supplychain_erp",
   experimental: {},
   turbopack: {
     rules: {
